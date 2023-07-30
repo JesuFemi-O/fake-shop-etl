6ab97b868dfdd60c189d1799fd2668f7a077c14a
JesuFemi-O <ogunwedeemmanuel@gmail.com>
2023-07-30 15:46:24 +0100

    first commit

---
+++ generator.py
@@ -0,0 +1,50 @@
+import os
+import csv
+import io
+import random
+import uuid
+from dotenv import load_dotenv
+from google.cloud import storage
+
+load_dotenv()
+
+def generate_customer_orders():
+    num_orders = random.randint(5, 30)
+    orders = []
+
+    for order_id in range(1, num_orders + 1):
+        customer_id = random.randint(1000, 9999)
+        product_id = random.randint(1, 100)
+        quantity = random.randint(1, 10)
+        total_amount = round(random.uniform(10, 100), 2)
+
+        order = [order_id, customer_id, product_id, quantity, total_amount]
+        orders.append(order)
+
+    return orders
+
+def upload_to_gcs(bucket_name, file_name, data, environment):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+
+    unique_id = str(uuid.uuid4())[:8]  # Generate a random 8-character UUID
+    # 
+    full_file_name = f"{environment.lower()}/{file_name}_{unique_id}.csv"
+
+    # Convert list of lists to CSV string
+    csv_string = io.StringIO()
+    csv_writer = csv.writer(csv_string)
+    csv_writer.writerow(['OrderID', 'CustomerID', 'ProductID', 'Quantity', 'TotalAmount'])
+    csv_writer.writerows(data)
+
+    # Upload the CSV string to GCS
+    blob = bucket.blob(full_file_name)
+    blob.upload_from_string(csv_string.getvalue(), content_type='text/csv')
+
+if __name__ == "__main__":
+    environment = os.environ.get('ENVIRONMENT', 'PROD')
+    file_name = 'customer_orders'
+
+    bucket_name = 'fake-shop-lake'
+    orders = generate_customer_orders()
+    upload_to_gcs(bucket_name, file_name, orders, environment)
---
+++ main.py
@@ -0,0 +1,61 @@
+import os
+from google.cloud import bigquery
+from google.cloud import storage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def process_file(event, context):
+    bucket_name = event['bucket']
+    file_name = event['name']
+
+    # Ignore files in the processed subfolder
+    if file_name.startswith('dev/processed/') or file_name.startswith('prod/processed/'):
+        return
+
+    # Determine the environment based on the file prefix
+    if file_name.startswith('dev/'):
+        environment = 'DEV'
+    elif file_name.startswith('prod/'):
+        environment = 'PROD'
+    else:
+        # Invalid file prefix, log an error or handle appropriately
+        return
+
+    # Load CSV file into BigQuery
+    bigquery_client = bigquery.Client()
+    dataset_id = 'fake_shop'
+    if environment == 'DEV':
+        dataset_id = f'dev_{dataset_id}'  # Prefix dataset_id with "dev_"
+    table_id = 'customer_orders'
+    dataset_ref = bigquery_client.dataset(dataset_id)
+    table_ref = dataset_ref.table(table_id)
+
+    job_config = bigquery.LoadJobConfig(
+        autodetect=True,
+        skip_leading_rows=1,
+        source_format=bigquery.SourceFormat.CSV,
+    )
+
+    uri = f"gs://{bucket_name}/{file_name}"
+    load_job = bigquery_client.load_table_from_uri(uri, table_ref, job_config=job_config)
+    load_job.result()
+
+    if environment == 'PROD':
+        # Move the file to the processed subfolder
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(bucket_name)
+        source_blob = bucket.blob(file_name)
+        
+        new_file_name = f"{environment.lower()}/processed/{file_name.split('/')[-1]}"
+        destination_blob = bucket.blob(new_file_name)
+        destination_blob.rewrite(source_blob)
+        source_blob.delete()
+
+if __name__ == "__main__":
+    # This is for local testing, but the function will be triggered by GCS events in the cloud
+    event_data = {
+        "bucket": "fake-shop-lake",
+        "name": "dev/customer_orders.csv"
+    }
+    process_file(event_data, None)
